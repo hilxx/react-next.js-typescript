05518c875ea077d859c240e906f29e825f5e89d0
이은정 <eunjung377@naver.com>
2026-08-07 09:43:43 +0900

    test

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/app/ui/global.css";
 import {inter} from "@/app/ui/fonts";
 // 루트 레이아웃에 추가하는 모든 UI 요소는 모든 페이지에서 공유됨
-
+// ss
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
